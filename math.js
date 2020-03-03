51cdc432b67e37de278aa0d5685821132365aba7
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-03-03 14:24:54 +1300

    Added math

---
+++ math.js
@@ -0,0 +1,19 @@
+var numberOne = 3;
+var numberTwo = 7;
+
+console.log(numberOne+numberTwo);
+
+var sum = numberOne+numberTwo;
+console.log("Sum =",sum);
+
+var sub = numberOne-numberTwo;
+console.log("Sub =",sub);
+
+var div = numberOne/numberTwo;
+console.log("Div =",div);
+
+var mult = numberOne*numberTwo;
+console.log("Mult =",mult);
+
+var rem = numberOne%numberTwo;
+console.log("Rem =",rem);
